cf01f14de99f8e798dab950f0776e9ec3a292d23
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2023-11-12 21:52:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿var costs = new int[] { 750, 1000, 1250, 1500, 2000, 2500, 3000 };
+
+for (int dailyClaims = 1; dailyClaims <= 7; dailyClaims++)
+{
+    Console.WriteLine($"{dailyClaims} daily claim(s):");
+    Console.WriteLine("XP");
+    int claimIndex = 0;
+    int totalPoints = 0;
+
+    for (int level = 0; level < 50; level++)
+    {
+        var claims = new int[dailyClaims];
+        int points = level / 10 + 1;
+        totalPoints += points;
+
+        //int Sum() => new int[7].Select((_, i) => i >= claims.Length ? 0 : claims[i])
+        //             .Zip(costs, (a, b) => a*b).Sum();
+
+        for (int i = 0; claims.Sum() < totalPoints; i++)
+            claims[i % dailyClaims]++;
+
+        Console.WriteLine("=" + string.Join('+', claims.Take(dailyClaims).Select((n, idx) => $"{n}*{costs[idx]}")));
+    }
+
+    Console.WriteLine();
+}
